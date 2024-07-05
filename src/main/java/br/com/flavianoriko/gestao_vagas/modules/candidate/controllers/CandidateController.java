062f1cca39d4e801ca9afeefe1779c4e4d295008
Norikinho <flavia.noriko.fukuda.portfolio@gmail.com>
2024-07-05 17:14:34 -0300

    validando se o usuario ja existe

--- src/main/java/br/com/flavianoriko/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/flavianoriko/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -3,7 +3,7 @@ package br.com.flavianoriko.gestao_vagas.modules.candidate.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.flavianoriko.gestao_vagas.modules.candidate.CandidateEntity;
-import br.com.flavianoriko.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.flavianoriko.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,11 +23,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidateController {
    
    @Autowired 
-   private CandidateRepository candidateRepository;
+   private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-      return this.candidateRepository.saveAndFlush(candidateEntity);
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+      try{
+         var result =  this.createCandidateUseCase.execute(candidateEntity);
+         return ResponseEntity.ok().body(result);
+      }
+      catch(Exception e)
+      {
+         return ResponseEntity.badRequest().body(e.getMessage());
+      }
+      //return this.candidateRepository.saveAndFlush(candidateEntity);
     }
 
     @GetMapping("/consulta")
