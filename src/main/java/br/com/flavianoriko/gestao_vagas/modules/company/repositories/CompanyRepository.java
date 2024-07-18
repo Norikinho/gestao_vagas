b7f344b8ad4232fb94cc5ddae56462a2852c746d
Norikinho <flavia.noriko.fukuda.portfolio@gmail.com>
2024-07-18 13:39:34 -0300

    parte de token

--- src/main/java/br/com/flavianoriko/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ src/main/java/br/com/flavianoriko/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -9,5 +9,6 @@ import java.util.Optional;
 
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID>{
- Optional<CompanyEntity>findByUsernameOrEmail(String email, String username);    
+ Optional<CompanyEntity>findByUsernameOrEmail(String username, String email);  
+ Optional<CompanyEntity> findByUsername(String username);
 }
